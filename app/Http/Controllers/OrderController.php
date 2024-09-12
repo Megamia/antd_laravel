fe0ed414406f3e0d58969c45cb864a311dafe6a5
Megamia <luudat214@gmaill.com>
2024-09-12 10:58:21 +0700

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,51 +11,6 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    // public function createOrder(Request $request)
-    // {
-    //     $data = $request->only('idDetailOrder', 'idInforUser', 'idVoucher', 'valueOrder');
-    //     $createOrders = [];
-    //     $failedOrders = [];
-
-    //     $totalValueVoucher = 0;
-    //     if (!empty($data['idVoucher'])) {
-    //         $idVoucherCodeValue = $data['idVoucher']['idVoucherCodeValue'] ?? null;
-    //         $idVoucherPromotionValues = $data['idVoucher']['idVoucherPromotionValue'] ?? [];
-
-    //         $valueVoucherCode = $idVoucherCodeValue ? VoucherCodeValue::where('id', $idVoucherCodeValue)->value('value') : 0;
-    //         $valueVoucherPromotion = !empty($idVoucherPromotionValues) ? VoucherPromotionValue::whereIn('id', $idVoucherPromotionValues)->sum('value') : 0;
-
-    //         $totalValueVoucher = $valueVoucherCode + $valueVoucherPromotion;
-    //     }
-
-    //     foreach ($data['idDetailOrder'] as $idDetailOrder) {
-    //         try {
-    //             $createOrder = Order::create([
-    //                 'idDetailOrder' => $idDetailOrder,
-    //                 'idVoucher' => $data['idVoucher'] ?? null,
-    //                 'idInforUser' => $data['idInforUser'],
-    //                 'valueVoucher' => $totalValueVoucher,
-    //                 'valueOrder' => $data['valueOrder'],
-    //             ]);
-    //             $createOrders[] = $createOrder;
-    //         } catch (\Exception $e) {
-    //             $failedOrders[] = [
-    //                 'idDetailOrder' => $idDetailOrder,
-    //                 'error' => $e->getMessage()
-    //             ];
-    //         }
-    //     }
-
-    //     if (count($failedOrders) > 0) {
-    //         return response()->json([
-    //             'status' => 0,
-    //             'message' => 'Some orders could not be created',
-    //             'failedOrders' => $failedOrders
-    //         ], 500);
-    //     }
-
-    //     return response()->json(['status' => 1, 'createOrders' => $createOrders]);
-    // }
     public function createOrderWithoutValue(Request $request)
     {
         $validated = $request->validate([
